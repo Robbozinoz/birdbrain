d4a24a8c210937e5ce9c441167650a7b72791a4b
Robbozinoz <Robbozinoz>
2019-10-16 16:00:23 +0800

    Final imagery sizing and insertion, colour config variable applied, final code tidy up and comments applied throughout site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,25 @@
-//Select  DOM Items
+//Select  DOM Items in the menu to control
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-//Set the initial state of the menu burger bar
+//Set the initial state of the menu to false(hidden)
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if (!showMenu) {
+        //Check for menu state and apply classes to DOM to close menu
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
-        //Set menu burger bar to true
+        //Set menu show menu binding to true
         showMenu = true;
     } else {
         menuBtn.classList.remove('close');
@@ -27,7 +28,7 @@ function toggleMenu() {
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
-        //Set menu burger bar to true
+        //Set menu show menu binding to false(default state)
         showMenu = false;
     }
 }
